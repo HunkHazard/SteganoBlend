6db8ac0306d774a373c2d433996fb746b50500fa
HussainSyed268 <snaqvi.bscs21seecs@seecs.edu.pk>
2023-12-21 02:12:04 +0500

    encryption-based Text encoding added

--- backend/algorithms/encode.py
+++ backend/algorithms/encode.py
@@ -1,4 +1,4 @@
-from utils.helperFunctions import determineImageType, displayImage
+from utils.helperFunctions import determineImageType, displayImage,text_to_binary,generate_key,encrypt_message,decrypt_message,hide_text_in_image,retrieve_text_from_image
 import sys
 import numpy as np
 import cv2
@@ -43,3 +43,78 @@ def multipleBitDecryption(encrypted_image, bit_shift):
         return decrypted_image
     else:
         raise ValueError("Unsupported image type")
+
+def multiBitTextEncryption(cover_image, text_to_hide, bit_shift, stop_character='\0'):
+    binary_text = text_to_binary(text_to_hide) + text_to_binary(stop_character)
+
+    width, height = cover_image.size
+
+    # Check if the text is too long for the image
+    if len(binary_text) > width * height * bit_shift:
+        raise ValueError("Text too long for the given image and bit shift")
+
+    # Iterate through each pixel
+    pixel_index = 0
+    for y in range(height):
+        for x in range(width):
+            pixel = list(cover_image.getpixel((x, y)))
+
+            for c in range(3):  # For RGB channels
+                if pixel_index < len(binary_text):
+                    # Modify the LSB of the pixel color values
+                    pixel[c] = pixel[c] & ~(1 << bit_shift) | (int(binary_text[pixel_index]) << bit_shift)
+                    pixel_index += 1
+
+            cover_image.putpixel((x, y), tuple(pixel))
+
+    return cover_image
+
+def multiBitTextDecryption(modified_image, bit_shift, stop_character='\0'):
+    width, height = modified_image.size
+
+    binary_text = ''
+    found_stop = False
+
+    # Retrieve text until the stop character is found
+    for y in range(height):
+        for x in range(width):
+            pixel = list(modified_image.getpixel((x, y)))
+
+            for c in range(3):  # For RGB channels
+                # Retrieve the LSB of the pixel color values
+                binary_text += str((pixel[c] & (1 << bit_shift)) >> bit_shift)
+
+
+                # Check for stop_character at the end of each character (8 bits)
+                if len(binary_text) >= 8 and binary_text[-8:] == text_to_binary(stop_character):
+                    found_stop = True
+                    break
+
+            if found_stop:
+                break
+
+        if found_stop:
+            break
+
+    # Remove the stop character from the binary text
+    if found_stop:
+        binary_text = binary_text[:-8]
+
+    # Padding the binary text to multiples of 8 for conversion
+    while len(binary_text) % 8 != 0:
+        binary_text += '0'
+
+    # Convert binary text back to characters
+    extracted_text = ''.join(chr(int(binary_text[i:i + 8], 2)) for i in range(0, len(binary_text), 8))
+    return extracted_text
+
+def keyBasedTextEncoding(original_image,message):
+    key = generate_key()
+    encrypted_message = encrypt_message(message, key)
+    modified_image = hide_text_in_image(original_image, encrypted_message)
+    return modified_image,key
+
+def keyBaseTextDecoding(retrieved_image,key):
+    retrieved_data = retrieve_text_from_image(retrieved_image)
+    decrypted_message = decrypt_message(retrieved_data.encode(), key)
+    return decrypted_message
--- backend/utils/helperFunctions.py
+++ backend/utils/helperFunctions.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+import string
+from cryptography.fernet import Fernet
 
 
 def displayImage(image, label='Image'):
@@ -18,8 +20,6 @@ def determineImageType(image):
         return 'grayscale'
     elif len(image.shape) == 3 and image.shape[2] == 3:
         return 'rgb'
-    # elif len(image.shape) == 3 and image.shape[2] == 3:
-    #     return 'rgb'
     else:
         raise ValueError("Unsupported image type")
 
@@ -29,3 +29,66 @@ def resizeImage(original_image, image_to_hide):
         return image_to_hide
     else:
         return cv2.resize(image_to_hide, (original_image.shape[1], original_image.shape[0]))
+    
+# Function to check if the text contains only English alphabets, digits, and common punctuation
+def is_valid_text(text):
+    valid_characters = string.ascii_letters + string.digits + string.punctuation + ' '
+    return all(char in valid_characters for char in text)
+
+
+# Function to convert text to binary
+def text_to_binary(text):
+    binary_text = ''.join(format(ord(char), '08b') for char in text)
+    return binary_text
+
+def binary_to_text(binary):
+    text = ''.join(chr(int(binary[i:i+8], 2)) for i in range(0, len(binary), 8))
+    return text
+
+def decrypt_message(encrypted_message, key):
+    try:
+        f = Fernet(key)
+        return f.decrypt(encrypted_message).decode()
+    except Exception as e:
+        return "Decryption failed: Invalid key or corrupted data"
+
+# Encryption and Decryption
+def generate_key():
+    return Fernet.generate_key()
+
+def encrypt_message(message, key):
+    f = Fernet(key)
+    return f.encrypt(message.encode())
+
+
+def hide_text_in_image(image, text_to_hide, bit_shift=1):
+    binary_text = text_to_binary(text_to_hide)
+    width, height = image.size
+
+    if len(binary_text) > width * height * 3 * bit_shift:
+        raise ValueError("Text too long for the given image and bit shift")
+
+    pixel_index = 0
+    for y in range(height):
+        for x in range(width):
+            pixel = list(image.getpixel((x, y)))
+            for c in range(3):
+                if pixel_index < len(binary_text):
+                    pixel[c] = pixel[c] & ~(1 << bit_shift) | (int(binary_text[pixel_index]) << bit_shift)
+                    pixel_index += 1
+            image.putpixel((x, y), tuple(pixel))
+    return image
+
+def retrieve_text_from_image(image, bit_shift=1):
+    binary_text = ''
+    width, height = image.size
+
+    for y in range(height):
+        for x in range(width):
+            pixel = list(image.getpixel((x, y)))
+            for c in range(3):
+                binary_text += str((pixel[c] & (1 << bit_shift)) >> bit_shift)
+                if len(binary_text) % 8 == 0 and len(binary_text) >= 8:
+                    if binary_text[-8:] == '00000000':
+                        return binary_to_text(binary_text[:-8])
+    return binary_to_text(binary_text)
